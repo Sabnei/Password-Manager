746ed206b3ce944546db4ddc9c827aa09164575b
Sabnei <sebasplay417@gmail.com>
2025-07-24 14:14:50 -0600

    Add password management functions with JSON storage

---
+++ manager.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+FILE_PATH = "storage.json"
+
+
+def load_data():
+    if not os.path.exists(FILE_PATH):
+        return {}
+    with open(FILE_PATH, "r") as file:
+        return json.load(file)
+
+
+def save_data(data):
+    with open(FILE_PATH, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def add_password(service, username, password):
+    data = load_data()
+    data[service] = {"username": username, "password": password}
+    save_data(data)
+
+
+def get_password(service):
+    return load_data().get(service)
+
+
+def list_services():
+    return list(load_data().keys())
+
+
+def delete_password(service):
+    data = load_data()
+    if service in data:
+        del data[service]
+        save_data(data)
+        return True
+    return False
